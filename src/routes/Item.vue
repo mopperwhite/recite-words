c27baad9f775ff2b8bf3fef2fa27dca518d6bcde
MopperWhite <mopperwhite@gmal.com>
2017-05-03 01:23:58 +0800

    233

--- src/routes/Item.vue
+++ src/routes/Item.vue
@@ -2,34 +2,44 @@
 div
     h1.text-center
         | {{answer.test}}
-    div(v-if="guessing")
-        template(v-for="st in selectable")
-            button.btn.btn-block.btn-default(@click="guess(st)")
-                | {{st && st.answer}}
-    div(v-else)
-        div(v-if="ture_answer")
-            h3.text-center.text-success
-                | 正确
-        div(v-else)
-            h3.text-center.text-danger
-                | 错误
-            h3.text-center.text-muted.small
-                del
-                    | {{wrong.answer}}
-                span
-                    | ({{wrong.test}})
+    div(v-if="skipping")
         h3.text-center.text-success
             | {{answer.answer}}
             span.small
                 | {{answer.counter}} / {{max_counter}}
         button.btn.btn-block.btn-success(@click="next_word")
-            | 下一个
-    span.process-bar-container.bg-muted.text-right
-        div.text-left.process-bar.bg-success(:style = '`width: ${counter*100 / records.length}%;`')
-            | {{counter}}
-        | {{records.length - counter}}
-    button.btn-warning.btn.btn-block(@click="next_word")
-        | 跳过
+                | 下一个
+    template(v-else)
+        div(v-if="guessing")
+            template(v-for="st in selectable")
+                button.btn.btn-block.btn-default(@click="guess(st)")
+                    | {{st && st.answer}}
+            
+            button.btn-warning.btn.btn-block(@click="skipping = true")
+                    | 跳过
+        div(v-else)
+            div(v-if="ture_answer")
+                h3.text-center.text-success
+                    | 正确
+            div(v-else)
+                h3.text-center.text-danger
+                    | 错误
+                h3.text-center.text-muted.small
+                    del
+                        | {{wrong.answer}}
+                    span
+                        | ({{wrong.test}})
+            h3.text-center.text-success
+                | {{answer.answer}}
+                span.small
+                    | {{answer.counter}} / {{max_counter}}
+            button.btn.btn-block.btn-success(@click="next_word")
+                | 下一个
+        span.process-bar-container.bg-muted.text-right
+            div.text-left.process-bar.bg-success(:style = '`width: ${counter*100 / records.length}%;`')
+                | {{counter}}
+            | {{records.length - counter}}
+        
     
 </template>
 <script>
@@ -46,6 +56,7 @@ export default {
             guessing: true,
             ture_answer: false,
             wrong: '',
+            skipping: false,
         }
     },
     methods: {
@@ -64,6 +75,7 @@ export default {
             }
         },
         next_word(){
+            this.skipping= false
             this.guessing = true
             let records = this.records
                 .map(e => {
